0e517e630cf9e0b0927258ab9feb1bfdb761c319
priyatc05 <pdtcdxb05@gmail.com>
2024-03-24 13:59:40 +0400

    added 3 products to rtdatabase

--- app/src/main/java/com/middlesex/quickart/MainActivity.java
+++ app/src/main/java/com/middlesex/quickart/MainActivity.java
@@ -10,10 +10,14 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.middlesex.quickart.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding;
@@ -30,33 +34,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        List<Product> productList = new ArrayList<>();
+        Product product1=new Product();
+        product1.setProductId("www222");
+        product1.setProductName("Laban");
+        product1.setProductPrice(3);
+        productList.add(product1);
+        Product product2=new Product();
+        product2.setProductId("xxx333");
+        product2.setProductName("lipstick");
+        product2.setProductPrice(4);
+        productList.add(product2);
+        Product product3 = new Product();
+        product3.setProductId("gfh37837");
+        product3.setProductName("Balloon");
+        product3.setProductPrice(1);
+        productList.add(product3);
         binding.button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Product product1=new Product();
-                product1.setProductId("www222");
-                product1.setProductName("Laban");
-                product1.setProductPrice(3);
-                Product product2=new Product();
-                product2.setProductId("xxx333");
-                product2.setProductName("lipstick");
-                product2.setProductPrice(4);
 
                 db = FirebaseDatabase.getInstance();
                 reference = db.getReference("productDetail");
-                reference.child(product1.getProductName()).setValue(product1).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Toast.makeText(MainActivity.this, "Sucessfully updated", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-
-
-
+                for(int i=0;i<productList.size();i++) {
+                    int finalI = i;
+                    reference.child(productList.get(i).getProductName()).setValue(productList.get(i)).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(MainActivity.this, "Sucessfully added to database: " + productList.get(finalI).getProductName(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
             }
         });
 
